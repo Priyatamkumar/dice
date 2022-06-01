2500702ae630948ab7f6ffd2c830244cd01a0aba
Priyatamkumar <93855467+Priyatamkumar@users.noreply.github.com>
2022-06-01 16:35:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
-var randomNumber1 = Math.floor( Math.random() * 6)+1;
-var randomdiceimg1 = "images/dice" + randomNumber1 + ".png";
-document.querySelector(".img1").setAttribute( "src", randomdiceimg1);
-var randomNumber2 = Math.floor( Math.random() * 6)+1;
-var randomdiceimg2 = "images/dice" + randomNumber2 + ".png";
-document.querySelector(".img2").setAttribute( "src", randomdiceimg2);
-if(randomNumber1==randomNumber2)
-{
+var randomnumber1= Math.floor(Math.random()*6)+1;
+var image1="images/dice"+randomnumber1+".png";
+var randomnumber2= Math.floor(Math.random()*6)+1;
+var image2="images/dice"+randomnumber2+".png";
+document.querySelector(".img1").setAttribute("src",image1);
+document.querySelector(".img2").setAttribute("src",image2);
+if(randomnumber1==randomnumber2)
     document.querySelector("h1").innerHTML="Draw!";
-}
-else 
-{  if(randomNumber1>randomNumber2)
-    {
-        document.querySelector("h1").innerHTML="🚩 Player 1 Wins!";
-    }
-    else{
-        document.querySelector("h1").innerHTML="Player 2 Wins! 🚩";
-    }
-}
+else if(randomnumber1>randomnumber2)
+    document.querySelector("h1").innerHTML="🚩Player1 Wins!";
+else
+    document.querySelector("h1").innerHTML="Player2 Wins!🚩";
